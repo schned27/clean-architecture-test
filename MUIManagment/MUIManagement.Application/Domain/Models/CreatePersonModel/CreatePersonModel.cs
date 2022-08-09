9e0b00da99eb560dcfe19fced8d852fbcaf72d62
von Burg <vonburgl@emea.roche.com>
2022-08-09 08:38:07 +0200

    change folder structure

---
+++ MUIManagment/MUIManagement.Application/Domain/Models/CreatePersonModel/CreatePersonModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MUIManagement.Application.Domain.Models.CreatePersonModel
+{
+    public class PersonModel
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
